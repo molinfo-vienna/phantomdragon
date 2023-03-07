6ea2d0e90d3b2c6f090cb6e89d2a147e6606650c
dragon3221 <christian@fellingers.net>
2023-03-07 16:40:10 +0100

    "final CASF update"

--- build/lib/phantomdragon/functions.py
+++ build/lib/phantomdragon/functions.py
@@ -252,7 +252,7 @@ class parameterCollector:
             self.scoretype = self.scoretype.replace("div","/")
         
 
-    def phantomtest(self, testing_features = 0, testing_scores = 0,loadpath=""):
+    def phantomtest(self, testing_features = 0, testing_scores = 0,loadpath="",confidence_level=0.9):
         if testing_features != 0:
             self.features_test = testing_features
         if testing_scores != 0:
@@ -271,7 +271,12 @@ class parameterCollector:
         self.scores_pre = scores_pre
 
         self.mse = mean_squared_error(self.scores_test, self.scores_pre)
-        self.r = round(pearsonr(self.scores_test,self.scores_pre).statistic, 6)
+        self.sd = statistics.stdev(self.scores_pre)
+        self.r = round(stats.pearsonr(self.scores_test,self.scores_pre).statistic, 6)
+        conf_int = stats.pearsonr(self.scores_test,self.scores_pre).confidence_interval(confidence_level=confidence_level)
+        conf_int_low = round(conf_int.low,3)
+        conf_int_high = round(conf_int.high,3)
+        self.conf_int = f"[{conf_int_low} ~ {conf_int_high}]"
         self.r_2 = round(r2_score(self.scores_test,self.scores_pre), 6)
     
     def plot_phantomtest(self,savepath):
