4846db3831c51d8368678fabed477e89aacd90ca
dragon3221 <christian@fellingers.net>
2023-07-03 11:03:32 +0200

    example script

---
+++ scripts/lazypredict.py
@@ -0,0 +1,18 @@
+from lazypredict.Supervised import LazyRegressor
+from sklearn import datasets
+from sklearn.utils import shuffle
+import numpy as np
+
+boston = datasets.load_boston()
+X, y = shuffle(boston.data, boston.target, random_state=13)
+X = X.astype(np.float32)
+
+offset = int(X.shape[0] * 0.9)
+
+X_train, y_train = X[:offset], y[:offset]
+X_test, y_test = X[offset:], y[offset:]
+
+reg = LazyRegressor(verbose=0, ignore_warnings=False, custom_metric=None)
+models, predictions = reg.fit(X_train, X_test, y_train, y_test)
+
+print(models)
